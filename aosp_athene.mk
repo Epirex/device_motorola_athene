238c931ce1be0f6b876ff0484720aa9ba033a58c
Esteban Guzzo <estebanguzzo@gmail.com>
2017-09-30 17:02:29 -0300

    athene: Define bootanimation res

--- aosp_athene.mk
+++ aosp_athene.mk
@@ -19,8 +19,7 @@ $(call inherit-product, vendor/aosp/common.mk)
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
